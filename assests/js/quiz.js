6fea1c38e15146d9c8b3a11c8dcd326db60bd81b
Rugger11 <82486396+zimmermanc6588@users.noreply.github.com>
2021-10-20 19:06:49 -0500

    Questions made

---
+++ assests/js/quiz.js
@@ -0,0 +1,27 @@
+var questions =[
+    {
+        question:"Which is not not a way JavaScript code can be involved in an HTML file?",
+        choices: ["Inline","Internal","Offset","External"],
+        answer: "Offset"
+            },
+    {
+question:"What is NaN in JavaScript?",
+choices: ["Not a Number","New attributes Number","Null and Not"],
+answer:"Not a Number"
+    },
+    {
+    question:"Is JavaScript a case-senstive language?",
+choices: ["True", "False"],
+answer:"True"
+    },
+    {
+        question: "What is a local Variable?",
+        choices: ["A varible that is visble everywhere in your code","Only visible within a function where it's defined","Only visble in local storage"],
+        answer:"Only visible within a function where it's defined"
+            }, 
+{
+    question:"Which company developed JavaScipt",
+    choices: ["Google","Java", "Netscape","Goodyear"],
+    answer:"Netscape"
+},
+]
