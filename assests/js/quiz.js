01af274a5a10679fdf223cc1183d6190cef08d9f
Rugger11 <82486396+zimmermanc6588@users.noreply.github.com>
2021-10-28 02:40:51 -0500

    times up window alert

--- assests/js/quiz.js
+++ assests/js/quiz.js
@@ -36,62 +36,64 @@ answer:"Knowledgeable in both the front end and back end of an application"
 }
 
 ];
-var questionsel=0
+// var questionsel=0
 var startBtn=document.querySelector("#start")
 const currenttime=document.querySelector("#timer");
-var time =75;
+var time =26;
 var timer;
 
 
 
-
-
-function displayQuestion(){
+function displayQuestionverb(){
     displayQuestion++;
 
-
-// var time =75;
-// var timer;
     if (displayQuestion>questions.length-1){
        quizDone();
         return;
     }
     var quizDone="<h2>"+questions[displayQuestion].question+"</h2>"
-   for (var i=0;i<questions[displayQuestion].choices.length;i++) {
+   for (var i=0;i<questions[displayQuestion].choices.length;i++)  {
+      var choiceBtn=`<button>"[CHOICE]"</button>`
+      console.log(choiceBtn)
     choiceBtn=choiceBtn.replace("[CHOICE]",questions[displayQuestion].choices[i]);
+    console.log(choiceBtn)
     if(questions[displayQuestion].choices[i]=== questions[displayQuestion].answer) {
         choiceBtn=choiceBtn.replace("[anwerchoice]","rightAnswer()"); 
     }else{
         choiceBtn=choiceBtn.replace("[anwerchoice]", "wrongAnswer()"); 
     } 
-    quiz+=choiceBtn
+    quizDone+=choiceBtn
 }
-document.getElementsByClassName("quiz").innerHTML=quiz;
+document.getElementsByClassName("quiz")[0].innerHTML=quizDone;
 }
 
 
 //this to start quiz
 
 startBtn.addEventListener("click", ()=>{
-    timeleft=75
-    var questionsel=0
+    timeleft=5;
+ 
+    // var questionsel=0
     var startBtn=document.querySelector("#start")
     const currenttime=document.querySelector("#timer");
+    
+    document.getElementById("timer").innerHTML=time+ " seconds";
 
-    document.getElementById("timer").innerHTML+"seconds;"
+    timer=setInterval(function() {
+    time--;
 
-quiztimer=setInterval(()=>{
-    time=time-1
-
-    // timeleft--;
+    
     currenttime.textContent=time;
+    time=time-1
+    document.getElementById("timer").innerHTML="Time left "+time+ " seconds";
     if(time<=0){
-        clearInterval(quizTimer);
+        clearInterval(timer);
+       
         window.alert("Time ran out, Game over");
-        displayQuestion();
+        over();
     }
 },1000);
-displayQuestion();
+displayQuestionverb();
 })
 
 
@@ -105,39 +107,58 @@ displayQuestion();
 var respondsEl=document.querySelector("#responds");
 
 function rightAnswer(){
-    respondsEl.textContent ="You are Correct!:"+questions[nextQuestion].answer;
-respondsEl.setAttribute("class","responds");
+    respondsEl.textContent ="You are Correct!:"+questions[displayQuestionverb].answer;
+respondsEl.setAttribute("class","disappear");
 setTimeout(function(){
-    respondsEl.setAttribute("class","hide");
-},0)
-displayQuestion();
+    respondsEl.setAttribute("class","disappear");
+},1500)
+displayQuestionverb();
 
 }
 
-function quizDone(){
-    clearInterval(quizTimer);
+function over(){
+    clearInterval(timer);
 
-var details=`
+var details=` 
  <h2>Finished</h2> 
-<p>Your score is ` + finalscore + `.</p>
+<p>Your score is  + finalscore + </p>
 <p>Enter your intials</p>
-<input type ="text" id="name" class="intials">
+<input type="text" id="scorer" class="intials" required>
 <button onclick="savescore()" class="savescorebtn" title="Submit Score">Submit</button>`;
    
    document.getElementById("start").innerHTML=details;
 }
     
-    
-//     var scoreinfo=~("savescore", "");
-//     <h2>~+localStorage.getItem</h2>
-// };
+    function winner() {
+     var details=`("savescore", "");
+   <h2>`+localStorage.getItem("scorer") + `Score is:</h2> `
+
+
+    }
 
 function resetGame(){
-    clearInterval(quizDone);
+    clearInterval(qtimer);
     displayQuestion=-1;
+    timeleft=0;
+    time=null;
+    document.getElementById("timeleft").innerHTML=timeleft+ "seconds";
+    window.location.reload();
+    document.getElementsById("start").innerHTML=details;
 }
-function getscore(){
+function clearscore(){
+    var details
     localStorage.setItem("score",finalscore);
-    localStorage.setItem("name")+`</h1><br ></br>
-    `
+    localStorage.setItem("scorer", "");
+    resetGame();
 };
+function wrongAnswer(){
+    
+} respondsEl.textContent ="Inncorrect"+questions[displayQuestionverb].answer;
+respondsEl.style.color = "red";
+respondsEl.style.fontWeight = "bold";
+respondsEl.setAttribute("class","disappear");
+setTimeout(function(){
+    respondsEl.setAttribute("class","disappear");
+},1500)
+displayQuestionverb();
+
